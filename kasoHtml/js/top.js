6df2d494dc88962bdc3830d05f94900e1660cb8e
BossLeeDragon <133199808+BossLeeDragon@users.noreply.github.com>
2024-10-14 09:24:11 +0800

    基础文件

---
+++ kasoHtml/js/top.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('.marquee').simplemarquee({
+		direction: 'left',
+		speed: 100,
+		cycles: 'Infinity',
+		space: 100,
+		delayBetweenCycles: 500,
+		handleResize: true,
+		handleHover: false
+	});
+});
